a252889c1c4a4a2b68e4081c8cc85b11dccdf637
caojuke <caojuke@126.com>
2022-07-17 21:55:54 +0800

    add VerificationCodeDTO

---
+++ api-passenger/src/main/java/com/msb/apipassenger/request/VerificationCodeDTO.java
@@ -0,0 +1,12 @@
+package com.msb.apipassenger.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerificationCodeDTO {
+    private String passengerPhone;
+}
